44ed4e869db37f6421274d8e37b5a48d44bb6d27
Alexander Adegbenro <52863002+AlexanderAdegbenro@users.noreply.github.com>
2022-05-31 15:27:04 -0400

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Alexander Adgebenro on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
