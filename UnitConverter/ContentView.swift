da74c7762d3f5c81af40f3a4293ebe1f3b93e02e
Alexander Adegbenro <52863002+AlexanderAdegbenro@users.noreply.github.com>
2022-06-08 14:19:54 -0400

    100 days of swiftui Project
    Pretty self explanatory!

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,14 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var Input = 100.0
+    @State private var InputUnit = "Meters"
+    @State private var OutputUnit = "Kilometers"
+    @FocusState private var inputIsFocused: Bool
+    
+    let units = ["Feet", "Kilometers" , "Meters", "Miles", "Yards"]
+    
+    var result: String {
+        let inputToMetersMultiplier: Double
+        let metersToOutputMultiplier: Double
+        
+        switch InputUnit {
+        case "Feet":
+            inputToMetersMultiplier = 0.3048
+        case "kiloMeters":
+            inputToMetersMultiplier = 1000
+            case "Miles":
+            inputToMetersMultiplier = 1609.34
+        case "Yards":
+            inputToMetersMultiplier = 0.9144
+        default:
+            inputToMetersMultiplier = 1.0
+        }
+        switch OutputUnit {
+        case "Feet":
+            metersToOutputMultiplier = 3.28084
+        case "kiloMeters":
+            metersToOutputMultiplier = 0.001
+            case "Miles":
+            metersToOutputMultiplier = 0.000621371
+        case "Yards":
+            metersToOutputMultiplier = 1.09361
+        default:
+            metersToOutputMultiplier = 1.0
+        }
+        let inputInMeters = Input * inputToMetersMultiplier
+        let output = inputInMeters * metersToOutputMultiplier
+        
+        
+        let outputString = output.formatted()
+        return "\(outputString) \(OutputUnit.lowercased())"
+        
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $Input, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
+                } header: {
+                    Text ("Enter Number")
+                }
+                Section {
+                    Picker("Convert from", selection: $InputUnit) {
+                        ForEach(units , id:\.self) {
+                            Text($0)
+                        }
+                    }
+                    
+                    
+                    Picker("Convert to", selection: $OutputUnit) {
+                        ForEach(units , id:\.self) {
+                            Text($0)
+                            
+                        }
+                    }
+                }
+                
+                Section {
+                    Text(result)
+                } header: {
+                    Text("Result")
+                }
+                
+                
+            }.navigationTitle("Unit Converter")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        
+                        Button("Done") {
+                            inputIsFocused = false
+                        }
+                    }
+                }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
